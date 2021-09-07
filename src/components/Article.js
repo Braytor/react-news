7d2b320560c11574998b8fe75b9958ef76eab27c
Antonin Koreta <66731662+Akoreta@users.noreply.github.com>
2021-09-07 11:39:06 +0200

    add form

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 const Article = ({article}) => {
+
+const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString('fr-FR',{
+        year:'numeric',
+        day:'numeric',
+        month:'long',
+        hour:'numeric',
+        minute:'numeric'
+    })
+    return newDate
+}
+
     return (
-        <div>
-
-            <p>Auteur :  {article.author}</p>
+        <div className="article">
+<div className="card-header">
+    <h3>Auteur :  {article.author} </h3>
+    <em>Posté le :  {dateParser(article.date)}</em>
+</div>
             <p>Contenu : {article.content}</p>
-            <p>Date : {article.date}</p> <br />
 
+            <div className="btn-container">
+                <button> Modifier</button>
+                <button> Supprimer</button>
+            </div>
 
         </div>
     );
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,42 +1,68 @@
-import React, {useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../components/Header";
 import Article from "../components/Article";
 import axios from "axios";
+
 const News = () => {
+    const [data, setData] = useState([]);
 
-    const [data,setData] = useState([]);
+    const [author, setAuthor] = useState('');
+    const [content, setContent] = useState('');
 
-const getData = () => {
-    axios.get('http://localhost:3003/articles')
-        .then((response) =>{
-            setData(response.data)
+    const getData = () => {
+        axios.get('http://localhost:3003/articles')
+            .then((response) => {
+                setData(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getData();
+    }, [data])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3003/articles', {
+            author,
+            content,
+            date:Date.now()
         })
-}
+            .then((res) => {
+                setContent('');
+                setAuthor('');
+            })
+    }
 
-useEffect(() =>{
-    getData();
-},[])
 
     return (
         <div>
             <Header></Header>
-<div className="news-container">
-    <h1>News</h1>
+            <div className="news-container">
+                <h1>News</h1>
 
-    <form>
-        <input type="text" placeholder="Nom"/>
-        <textarea placeholder="Message"> </textarea>
-        <input type="submit" value="Envoyer"/>
-    </form>
+                <form onSubmit={(e) => {
+                    handleSubmit(e)
+
+                }}>
+                    <input type="text" onChange={(e) => {
+                        setAuthor(e.target.value);
+                    }} placeholder="Nom" value={author}/>
+                    <textarea placeholder="Message" onChange={(e => setContent(e.target.value))} value={content}> </textarea>
+                    <input type="submit" value="Envoyer"/>
+                </form>
 
 
-    <ul>
-        {data.map((article)=>{
-            return <Article key={article.id} article={article} />
-        })}
-    </ul>
+                <ul>
+                    {data
+                        .sort((a, b) => {
+                            return b.date - a.date
+                        })
+                        .map((article) => {
+                            return <Article key={article.id} article={article}/>
+                        })}
+                </ul>
 
-</div>
+            </div>
         </div>
     );
 };
